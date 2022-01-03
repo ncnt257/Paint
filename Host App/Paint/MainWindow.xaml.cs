9b836c3edf1697d708883033d874c0fa5216905e
Tung Nguyen <ncnt257@gmail.com>
2022-01-03 19:21:00 +0700

    install ribbon

--- Host App/Paint/MainWindow.xaml.cs
+++ Host App/Paint/MainWindow.xaml.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Controls.Primitives;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
@@ -12,17 +13,41 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Fluent;
 
 namespace Paint
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : RibbonWindow
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+        void onDragDelta(object sender, DragDeltaEventArgs e)
+        {
+            //Move the Thumb to the mouse position during the drag operation
+            double yadjust = myCanvasStretch.Height + e.VerticalChange;
+            double xadjust = myCanvasStretch.Width + e.HorizontalChange;
+            if ((xadjust >= 0) && (yadjust >= 0))
+            {
+                myCanvasStretch.Width = xadjust;
+                myCanvasStretch.Height = yadjust;
+                Canvas.SetLeft(myThumb, Canvas.GetLeft(myThumb) +
+                                        e.HorizontalChange);
+                Canvas.SetTop(myThumb, Canvas.GetTop(myThumb) +
+                                       e.VerticalChange);
+            }
+        }
+        void onDragStarted(object sender, DragStartedEventArgs e)
+        {
+            myThumb.Background = Brushes.Orange;
+        }
+        void onDragCompleted(object sender, DragCompletedEventArgs e)
+        {
+            myThumb.Background = Brushes.Blue;
         }
     }
 }
