710589c4b6e25e99a6f9003110a9656cac407bb0
trandactoan <73386087+trandactoan@users.noreply.github.com>
2022-01-15 12:25:16 +0700

    Save and read image file
    -The image is file with .png, .jpg, .bmp extend
    -Save new image file
    -Save open file
    -Load image file as background

--- Host App/Paint/MainWindow.xaml.cs
+++ Host App/Paint/MainWindow.xaml.cs
@@ -1,24 +1,20 @@
-﻿using System;
+﻿using Contract;
+using Fluent;
+using Gma.System.MouseKeyHook;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Controls.Primitives;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using Contract;
-using Fluent;
-using Gma.System.MouseKeyHook;
+using Color = System.Windows.Media.Color;
+using Point = System.Windows.Point;
 
 
 namespace Paint
@@ -28,6 +24,8 @@ namespace Paint
     /// </summary>
     public partial class MainWindow : RibbonWindow
     {
+        public static string FilePath = "";
+        public static string FileName = Path.GetFileName(FilePath);
         private IMouseEvents _hook = Hook.GlobalEvents();
         bool _isDrawing = false;
         List<IShape> _shapes = new List<IShape>();
@@ -37,7 +35,7 @@ namespace Paint
             new Dictionary<string, IShape>();
 
         //Properties menu
-        new List<DoubleCollection> StrokeTypes = new List<DoubleCollection>() { new DoubleCollection() {1,0}, new DoubleCollection() { 6, 1 }, new DoubleCollection() { 1 }, new DoubleCollection() { 6, 1, 1, 1 } };
+        new List<DoubleCollection> StrokeTypes = new List<DoubleCollection>() { new DoubleCollection() { 1, 0 }, new DoubleCollection() { 6, 1 }, new DoubleCollection() { 1 }, new DoubleCollection() { 6, 1, 1, 1 } };
         public MainWindow()
         {
             InitializeComponent();
@@ -47,7 +45,7 @@ namespace Paint
             _hook.MouseMove += Hook_MouseMove;
             _hook.MouseUp += Hook_MouseUp;
         }
-        
+
 
         private void Canvas_MouseDown(object sender,
             MouseButtonEventArgs e)
@@ -70,9 +68,9 @@ namespace Paint
             var temp = e.Location;
             Point screenPos = new Point(temp.X, temp.Y);
             Point pos = DrawCanvas.PointFromScreen(screenPos);
-            
-            
-            
+
+
+
             CoordinateLabel.Content = $"{Math.Ceiling(pos.X)}, {Math.Ceiling(pos.Y)}px";
             if (pos.X < 0 || pos.X > DrawCanvas.ActualWidth || pos.Y < 0 || pos.Y >= DrawCanvas.ActualHeight)
             {
@@ -101,7 +99,8 @@ namespace Paint
         }
         private void Hook_MouseUp(object sender, System.Windows.Forms.MouseEventArgs e)
         {
-            if(_isDrawing){
+            if (_isDrawing)
+            {
                 _isDrawing = false;
 
                 var temp = e.Location;
@@ -145,7 +144,7 @@ namespace Paint
             {
                 if (dll.Name == "ControlzEx.dll") continue;
                 var assembly = Assembly.LoadFile(dll.FullName);
-                
+
                 var types = assembly.GetTypes();
 
                 foreach (var type in types)
@@ -172,7 +171,7 @@ namespace Paint
                     SizeDefinition = "Small",
                     GroupName = "Shape",
                     Tag = shape.Name,
-                    
+
                 };
                 button.Click += prototypeButton_Click;
                 Shape.Items.Add(button);
@@ -197,18 +196,127 @@ namespace Paint
 
         private void buttonSave_Click(object sender, RoutedEventArgs e)
         {
-
+            if (FilePath == "")
+            {
+                buttonSaveAs_Click(sender, e);
+                return;
+            }
+            string ext = Path.GetExtension(FilePath);
+            Debug.Write(ext);
+            DrawCanvas.UpdateLayout();
+            CreateBitmapFromVisual(DrawCanvas, FilePath, ext);
         }
 
         private void buttonSaveAs_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.DefaultExt = "png";
+            saveFileDialog.Filter = "PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|JPG Files (*.jpg)|*.jpg";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                FilePath = saveFileDialog.FileName;
+                switch (saveFileDialog.FilterIndex)
+                {
+                    case 1:
+                        {
+                            CreateBitmapFromVisual(DrawCanvas, saveFileDialog.FileName, ".png");
+                            break;
+                        }
+                    case 2:
+                        {
+                            CreateBitmapFromVisual(DrawCanvas, saveFileDialog.FileName, ".bmp");
+                            break;
+                        }
+                    case 3:
+                        {
+                            CreateBitmapFromVisual(DrawCanvas, saveFileDialog.FileName, ".jpg");
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
         }
 
         private void buttonOpen_Click(object sender, RoutedEventArgs e)
         {
-            
-            
+            OpenFileDialog browseDialog = new OpenFileDialog();
+            browseDialog.Filter = "PNG Files (*.png)|*.png|BMP Files (*.bmp)|*.bmp|JPG Files (*.jpg)|*.jpg";
+            browseDialog.FilterIndex = 1;
+            browseDialog.Multiselect = false;
+            if (browseDialog.ShowDialog() != true)
+            {
+                return;
+            }
+            FilePath = browseDialog.FileName;
+
+            //need to fix right here
+            // ImageBrush ib = new ImageBrush();
+            // BitmapImage inputFile = new BitmapImage(new Uri(FilePath, UriKind.RelativeOrAbsolute));
+            // ib.ImageSource = inputFile;
+            // DrawCanvas.Background = ib;
+
+            MemoryStream ms = new MemoryStream();
+            BitmapImage bi = new BitmapImage();
+            byte[] bytArray = File.ReadAllBytes(FilePath);
+            ms.Write(bytArray, 0, bytArray.Length); ms.Position = 0;
+            bi.BeginInit();
+            bi.StreamSource = ms;
+            bi.EndInit();
+            ImageBrush ib = new ImageBrush();
+            ib.ImageSource = bi;
+            DrawCanvas.Background = ib;
+        }
+
+        void CreateBitmapFromVisual(Visual target, string filename, string filerType)
+        {
+            if (target == null)
+                return;
+
+            Rect bounds = VisualTreeHelper.GetDescendantBounds(target);
+
+            RenderTargetBitmap rtb = new RenderTargetBitmap((Int32)bounds.Width, (Int32)bounds.Height, 96, 96, PixelFormats.Pbgra32);
+
+            DrawingVisual dv = new DrawingVisual();
+
+            using (DrawingContext dc = dv.RenderOpen())
+            {
+                VisualBrush vb = new VisualBrush(target);
+                dc.DrawRectangle(vb, null, new Rect(new Point(), bounds.Size));
+            }
+
+            rtb.Render(dv);
+            switch (filerType)
+            {
+                case ".png":
+                    PngBitmapEncoder png = new PngBitmapEncoder();
+
+                    png.Frames.Add(BitmapFrame.Create(rtb));
+                    using (Stream stm = File.Create(filename))
+                    {
+                        png.Save(stm);
+                    }
+                    break;
+                case ".bmp":
+                    BitmapEncoder bmp = new BmpBitmapEncoder();
+                    bmp.Frames.Add(BitmapFrame.Create(rtb));
+                    using (Stream stm = File.OpenWrite(filename))
+                    {
+                        bmp.Save(stm);
+                    }
+
+                    break;
+                case ".jpg":
+                    JpegBitmapEncoder jpg = new JpegBitmapEncoder();
+                    jpg.Frames.Add(BitmapFrame.Create(rtb));
+                    using (Stream stm = File.OpenWrite(filename))
+                    {
+                        jpg.Save(stm);
+                    }
+                    break;
+                default: break;
+            }
+
         }
         //private void Canvas_MouseMove(object sender, MouseEventArgs e)
         //{
@@ -295,9 +403,9 @@ namespace Paint
         {
             _hook.MouseMove -= Hook_MouseMove;
             _hook.MouseUp -= Hook_MouseUp;
-            
+
         }
 
-        
+
     }
 }
